358e699ed29b5cd4e44e414468c04258d12c116f
Victorcalwin <victorcalwin@gmail.com>
2023-02-01 20:50:56 +0530

    deploy purpose

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/books", router); // localhost:5000/books
 
-mongoose
-  .connect(
-    "mongodb+srv://admin:victorcalwin@cluster0.lu1vu4w.mongodb.net/?retryWrites=true&w=majority"
-  )
+mongoose.connect("mongodb+srv://admin:victorcalwin@cluster0.lu1vu4w.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("Connected To Database"))
   .then(() => {
     app.listen(5000);
